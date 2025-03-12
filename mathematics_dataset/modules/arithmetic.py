bed643a1617de9334d8f66ffb9f71def2169df06
khalidalt <ekh@protonmail.com>
2025-03-12 15:01:23 +0300

    fix typo

--- mathematics_dataset/modules/arithmetic.py
+++ mathematics_dataset/modules/arithmetic.py
@@ -1,4 +1,4 @@
-d# Copyright 2018 DeepMind Technologies Limited.
+# Copyright 2018 DeepMind Technologies Limited.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,7 +11,6 @@ d# Copyright 2018 DeepMind Technologies Limited.
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
 """Arithmetic, e.g., "calculate 2+3"."""
 
 from __future__ import absolute_import
